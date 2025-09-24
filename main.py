e9a3858a17fd06e470a160f1d239d165d8f9e886
Abhisek Mohanty <mabhisek970@gmail.com>
2025-09-24 08:20:54 +0530

    some changes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 row=5
-for i in range(5):
-    for j in range(5):
-        print(f"({row},{j})",end=" ")
-    row-=1
+for i in range(1, 6):
+    for j in range(1, 6):
+        print(f"{i*j:4}", end="")
     print()

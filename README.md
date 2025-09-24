e9ac3f3622c66d05506a712241b6211428fa559f
Abhisek Mohanty <mabhisek970@gmail.com>
2025-09-24 08:15:01 +0530

    Newly doing

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # git_trails
 It is just a trail version to learn end to end
+Newly built stuffs
---
+++ main.py
@@ -0,0 +1,6 @@
+row=5
+for i in range(5):
+    for j in range(5):
+        print(f"({row},{j})",end=" ")
+    row-=1
+    print()
